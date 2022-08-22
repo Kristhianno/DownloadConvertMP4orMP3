69f633346b0a43c8d01f3c2adadf62f9829cea6a
Efraim Kristhianno <ekriator@gmail.com>
2022-08-22 14:43:48 -0300

    TENTANDO FAZER UM EXECUTAVEL

---
+++ setup.py
@@ -0,0 +1,10 @@
+import cx_Freeze
+
+executables = [cx_Freeze.Executable('programa.py')]
+
+cx_Freeze.setup(
+    name="Programa MP4 to MP3",
+    options={'build_exe': {'packages': ['pytube', 'moviepy']}},
+
+    executables=executables    
+)
